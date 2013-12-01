ef73f21e288eb743c9c4e03dac1eac5fe1bddfa0
dmishin <shintyakov@gmail.com>
2013-12-01 04:03:40 +0400

    Installaton script written

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from distutils.core import setup
+import sys
+if sys.version_info >= (3, 0):
+    tkinter = "tkinter"
+else:
+    tkinter = "Tkinter"
+    
+setup(name='pick-geometry',
+      version='1.0',
+      description='GUI tool to query location on an image from user',
+      author='Dmitry Shintyakov',
+      author_email='shintyakov@gmail.com',
+      url='https://github.com/dmishin/pick-geometry',
+      packages=[],
+      scripts=['pick_geometry'],
+      license='MIT',
+      requires=["PIL",tkinter]
+)
